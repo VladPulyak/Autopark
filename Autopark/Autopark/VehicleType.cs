1bcdd7e976b5d1fa0b2b68690a3a8c1c0ea104b4
Vlad Pulyak <vlad.pulyak03@mail.ru>
2023-02-22 19:46:24 +0300

    Create autopark project

---
+++ Autopark/Autopark/VehicleType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autopark
+{
+    internal class VehicleType
+    {
+        public VehicleType()
+        {
+
+        }
+
+        public VehicleType(string typeName, double taxCoefficient)
+        {
+            TypeName = typeName;
+            TaxCoefficient = taxCoefficient;
+        }
+
+        public string TypeName { get; set; }
+        public double TaxCoefficient { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"TypeName = {TypeName}");
+            Console.WriteLine($"TaxCoefficient = {TaxCoefficient}");
+        }
+
+        public override string ToString()
+        {
+            string resultString = $"{TypeName}, {TaxCoefficient}";
+            Console.WriteLine(resultString);
+            return resultString;
+        }
+    }
+}
